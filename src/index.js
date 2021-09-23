f4bf7dbebe729af85b89c23839fce188be2d9119
Juan Lia <liajuancruz@gmail.com>
2021-09-23 16:52:17 -0300

    Ultimas modif

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import { saludar } from './js/componentes';
-import './styles.css';
+const { Client, RichEmbed } = require('discord.js');
+const { intervaloSLPprice } = require('./js/funciones');
+const token = 'ODkwMzg0NDQxODYyNjUxOTA0.YUvBIQ.qVthIYmCx5wwwelejuvjUqmdjwA';
 
-const nombre = 'Fernando';
-saludar(nombre);
+const client = new Client();
+// Inicia bot
+client.on('ready', () => {
+  console.log('Bot Now connected!');
+  console.log('Logged In as', client.user.tag);
+  client.user.setStatus('online');
+  intervaloSLPprice(client)
+});
+
+module.exports= {
+  client
+}
